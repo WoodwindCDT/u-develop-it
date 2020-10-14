2362aa1f3cb9fe0f71944fd7b91a3c171c290b5a
Ender <woodwind.turbeville@gmail.com>
2020-10-14 18:29:08 -0500

    Adjusted syntax to load db than Server

--- server.js
+++ server.js
@@ -22,6 +22,9 @@ app.use((req, res) => {
   res.status(404).end();
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// Start server after DB connection
+db.on('open', () => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
 });
