689104ace6eea5135534e4c0db327a0da2592ae8
Ender <woodwind.turbeville@gmail.com>
2020-10-15 17:39:13 -0500

    Added new app requests to delete, get, and put new data into db

--- server.js
+++ server.js
@@ -18,6 +18,27 @@ const db = new sqlite3.Database('./db/election.db', err => {
     console.log('Connected to the election database.');
 });
 
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+  const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id`;
+  const params = [];
+  db.all(sql, params, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
 // Get single candidate
 app.get('/api/candidates/:id', (req, res) => {
   const sql = `SELECT candidates.*, parties.name 
@@ -40,13 +61,28 @@ app.get('/api/candidates/:id', (req, res) => {
   });
 });
 
-// Get all candidates
-app.get('/api/candidates', (req, res) => {
-  const sql = `SELECT candidates.*, parties.name 
-             AS party_name 
-             FROM candidates 
-             LEFT JOIN parties 
-             ON candidates.party_id = parties.id`;
+app.put('/api/candidate/:id', (req, res) => {
+  const sql = `UPDATE candidates SET party_id = ? 
+               WHERE id = ?`;
+  const params = [req.body.party_id, req.params.id];
+
+  db.run(sql, params, function(err, result) {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: req.body,
+      changes: this.changes
+    });
+  });
+});
+
+// To get all parties
+app.get('/api/parties', (req, res) => {
+  const sql = `SELECT * FROM parties`;
   const params = [];
   db.all(sql, params, (err, rows) => {
     if (err) {
@@ -57,6 +93,23 @@ app.get('/api/candidates', (req, res) => {
     res.json({
       message: 'success',
       data: rows
+    });
+  });
+});
+
+// To get a single party from its corresponding ID
+app.get('/api/party/:id', (req, res) => {
+  const sql = `SELECT * FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+  db.get(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+
+    res.json({
+      message: 'success',
+      data: row
     });
   });
 });
@@ -78,11 +131,31 @@ app.delete('/api/candidates/:id', (req, res) => {
   });
 });
 
+// To delete a party
+app.delete('/api/party/:id', (req, res) => {
+  const sql = `DELETE FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+  db.run(sql, params, function(err, result) {
+    if (err) {
+      res.status(400).json({ error: res.message });
+      return;
+    }
+
+    res.json({ message: 'successfully deleted', changes: this.changes });
+  });
+});
+
 // Create a candidate
 app.post('/api/candidates', ({ body }, res) => {
   const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
   if (errors) {
     res.status(400).json({ error: errors });
+    return;
+  }
+  const errors1 = inputCheck(req.body, 'party_id');
+
+  if (errors1) {
+    res.status(400).json({ error: errors1 });
     return;
   }
   const sql = `INSERT INTO candidates (first_name, last_name, industry_connected) 
